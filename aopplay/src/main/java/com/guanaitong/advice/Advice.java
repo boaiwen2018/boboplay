60a0d26ac5f0340f95ea730318585caf1fa77242
xiaobo.li <xiaobo.li>
2018-10-12 14:09:38 +0800

    手动实现aop 模拟spring+asectj

--- aopplay/src/main/java/com/guanaitong/advice/Advice.java
+++ aopplay/src/main/java/com/guanaitong/advice/Advice.java
@@ -7,22 +7,6 @@ public abstract class Advice {
     protected Object aopObj;
     protected Method aopMethod;
 
-//    public Object getAopObj() {
-//        return aopObj;
-//    }
-//
-//    public void setAopObj(Object aopObj) {
-//        this.aopObj = aopObj;
-//    }
-//
-//    public Method getAopMethod() {
-//        return aopMethod;
-//    }
-
-    public void setAopMethod(Method aopMethod) {
-        this.aopMethod = aopMethod;
-    }
-
     public Advice(Object aopObj, Method aopMethod){
         this.aopObj = aopObj;
         this.aopMethod = aopMethod;
