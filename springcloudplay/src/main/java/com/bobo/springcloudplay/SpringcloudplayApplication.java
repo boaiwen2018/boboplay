6e8f9602d8e4328300828c8d404e4beb70d2a6ff
菠菜 <451013525@qq.com>
2019-01-10 16:08:50 +0800

    1.项目整理 2.增加springcloud模块

---
+++ springcloudplay/src/main/java/com/bobo/springcloudplay/SpringcloudplayApplication.java
@@ -0,0 +1,14 @@
+package com.bobo.springcloudplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudplayApplication.class, args);
+    }
+
+}
+
